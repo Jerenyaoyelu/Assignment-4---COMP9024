15b9bfc13be3d465d2721c63327481028623422c
YAOYELU <luyaoyejeren@gmail.com>
2019-04-27 19:25:43 +1000

    [finish createEmptyGraph and InsertEdge functions]yet to test

--- MyGraph.c
+++ MyGraph.c
@@ -3,7 +3,8 @@
 #include <assert.h>
 
 // A vertex is a 2D point
-typedef struct Vertex { int x; // x-coordinate
+typedef struct Vertex { 
+	int x; // x-coordinate
 	int y; // y-coordinate
 } Vertex;
 // each edge is a pair of vertices (end-points)
@@ -11,30 +12,136 @@ typedef struct Edge {
 	Vertex *p1; // first end point
 	Vertex *p2; // second end point
 } Edge;
+// A vertex node stores a vertex and other information, and you need to expand this type
 typedef struct VertexNode {
 	Vertex *v;
+	VertexNode *next;
+	// for disdinguish if it is a isolated vertex
+	int degree;
 } VertexNode;
 typedef struct GraphRep *Graph;
 typedef struct GraphRep { // graph header
-	VertexNode *vertices; // an array of vertices or a linked list of vertices  
+	VertexNode **vertices; // an array of vertices or a linked list of vertices  
 	int nV; // #vertices
 	int nE; // #edges
 } GraphRep;
-// A vertex node stores a vertex and other information, and you need to expand this type
+
 
 //The above types serve as a starting point only. You need to expand them and add more types. 
 // Watch the lecture video between 7:50pm-8:20 or so for my discussion about this assignment    
 
-// Add the time complexity analysis of CreateEmptyGraph() here
-Graph CreateEmptyGraph()
-{
-	
+VertexNode *NewVertexNode(Vertex *V){
+	VertexNode *newNode = (VertexNode *)malloc(sizeof(VertexNode));
+	assert(newNode!=NULL);
+	newNode->degree = 0;
+	newNode->next = NULL;
+	newNode->v = (Vertex *)malloc(sizeof(Vertex));
+	newNode->v = V;
+	return newNode;
 }
 
+// Add the time complexity analysis of CreateEmptyGraph() here
+//assume the miaximum size of array is 100
+Graph CreateEmptyGraph()
+{
+	Graph newg = (GraphRep *)malloc(sizeof(GraphRep));
+	assert(newg != NULL);
+	newg->vertices =(VertexNode **)malloc(100*sizeof(VertexNode *));
+	newg->nV = 100;
+	newg->nE = 0;
+	assert(newg->vertices!=NULL);
+	for(int i = 0; i< 100;i++){
+		newg->vertices[i] = NULL;
+	}
+	return newg;
+}
 // Add the time complexity analysis of InsertEdge() here
 int InsertEdge(Graph g, Edge *e)
 {
-	
+	//create new graph nodes
+	VertexNode *new1 = NewVertexNode(e->p1);
+	VertexNode *new2 = NewVertexNode(e->p2);
+	//update the degree
+	new1->degree++;
+	new2->degree++;
+	//it is an empty graph
+	if(g->nE == 0){
+		//insert the first vertexNode
+		g->vertices[0] = new1;
+		g->vertices[0]->next = new2;
+		g->vertices[1] = new2;
+		g->vertices[1]->next = new1;
+		g->nE ++;
+		g->nV = g->nV + 2;
+		return 1;
+	}
+	int j = 0;
+	VertexNode *crt;
+	//label to show that if links are added in both vertice side and also indicates the three cases below.
+	//{1,1} means both vertices are existing ones but the edge is new, and it has been added during the while loop, return 1
+	//{1,0}/{0,1} means only one vertex is new and the edge is new, the new vertex and new edge(the link on the new vertex side) have to be added after the while loop, return 1 
+	//{0,0}:
+		//if the function does not end up within the while loop, this means both are new vertices, the vertices and edge are needed to be added after the loop,return 1
+		//if the function ends up within the while loop, this means both are existing vertices and so is the edge,return 0
+	int isExisting[2] = {0,0};
+	//both vertices are existing, if this function ends in this loop
+	while(g->vertices[j] != NULL){
+		//add link on p1 side
+		if(
+			g->vertices[j]->v->x == e->p1->x && 
+			g->vertices[j]->v->y == e->p1->y
+		){
+			crt = g->vertices[j];
+			//to see if it exists or not
+			while(crt != NULL){
+				//the edge is existing in the graph
+				if(crt->v->x == e->p2->x && crt->v->y == e->p2->y){return 0;}
+				crt = crt->next;
+			}
+			//it is a new edge
+			g->vertices[j]->next = new2;
+			isExisting[0] = 1;
+		}
+		//add link on p2 side
+		if(
+			g->vertices[j]->v->x == e->p2->x && 
+			g->vertices[j]->v->y == e->p2->y
+		){
+			crt = g->vertices[j];
+			while(crt != NULL){
+				//the edge is existing in the graph
+				if(crt->v->x == e->p1->x && crt->v->y == e->p1->y){return 0;}
+				crt = crt->next;
+			}
+			//it is a new edge
+			g->vertices[j]->next = new1;
+			isExisting[1] = 1;
+		}
+		//when the links are added in both side, there is no need to continue to go through the loop.
+		if(isExisting[0] == 1 && isExisting[1] == 2){
+			g->nE++;
+			return 1;
+		}
+		j++;
+	}
+	//one vertices is existing, and another one is new which needed to be added into the array
+	if(isExisting[0] == 1 && isExisting[1] == 0){
+		g->vertices[j] = new2;
+		g->vertices[j]->next = new1;
+		g->nV++;
+	}else if(isExisting[0] == 0 && isExisting[1] == 1){
+		g->vertices[j] = new1;
+		g->vertices[j]->next = new2;
+		g->nV++;
+	}else{//both are new vertices
+		g->vertices[j] = new1;
+		g->vertices[j]->next = new2;
+		g->vertices[j+1] = new2;
+		g->vertices[j+1]->next = new1;
+		g->nV = g->nV + 2;
+	}
+	g->nE++;
+	return 1;
 }
 
 // Add the time complexity analysis of DeleteEdge() here
