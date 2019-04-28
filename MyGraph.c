42e57d409f386ed066251a8a29471e2f027669fd
YAOYELU <luyaoyejeren@gmail.com>
2019-04-28 11:48:45 +1000

    [fix logical bugs in insertedge]

--- MyGraph.c
+++ MyGraph.c
@@ -15,7 +15,7 @@ typedef struct Edge {
 // A vertex node stores a vertex and other information, and you need to expand this type
 typedef struct VertexNode {
 	Vertex *v;
-	VertexNode *next;
+	struct VertexNode *next;
 	// for disdinguish if it is a isolated vertex
 	int degree;
 } VertexNode;
@@ -36,6 +36,7 @@ VertexNode *NewVertexNode(Vertex *V){
 	newNode->degree = 0;
 	newNode->next = NULL;
 	newNode->v = (Vertex *)malloc(sizeof(Vertex));
+	assert(newNode->v !=NULL);
 	newNode->v = V;
 	return newNode;
 }
@@ -47,36 +48,45 @@ Graph CreateEmptyGraph()
 	Graph newg = (GraphRep *)malloc(sizeof(GraphRep));
 	assert(newg != NULL);
 	newg->vertices =(VertexNode **)malloc(100*sizeof(VertexNode *));
-	newg->nV = 100;
-	newg->nE = 0;
 	assert(newg->vertices!=NULL);
-	for(int i = 0; i< 100;i++){
-		newg->vertices[i] = NULL;
-	}
+	newg->nV = 0;
+	newg->nE = 0;
+	// for(int i = 0; i< 100;i++){
+	// 	printf("(tt)\n");
+	// 	newg->vertices[i] = NULL;
+	// }
 	return newg;
 }
 // Add the time complexity analysis of InsertEdge() here
 int InsertEdge(Graph g, Edge *e)
 {
-	//create new graph nodes
-	VertexNode *new1 = NewVertexNode(e->p1);
-	VertexNode *new2 = NewVertexNode(e->p2);
-	//update the degree
-	new1->degree++;
-	new2->degree++;
+	// //create new graph nodes
+	// VertexNode *new1 = NewVertexNode(e->p1);
+	// VertexNode *new2 = NewVertexNode(e->p2);
+	// //update the degree
+	// new1->degree++;
+	// new2->degree++;
 	//it is an empty graph
 	if(g->nE == 0){
 		//insert the first vertexNode
-		g->vertices[0] = new1;
-		g->vertices[0]->next = new2;
-		g->vertices[1] = new2;
-		g->vertices[1]->next = new1;
+		g->vertices[0] = NewVertexNode(e->p1);
+		g->vertices[0]->degree++;
+		//if use the new2 directly, it eventually will create a circle after this if statement
+		g->vertices[0]->next = NewVertexNode(e->p2);
+		g->vertices[0]->next->degree++;
+		g->vertices[1] = NewVertexNode(e->p2);
+		g->vertices[1]->degree++;
+		//if use the new1 directly, it eventually will create a circle after this if statement
+		g->vertices[1]->next = NewVertexNode(e->p1);
+		g->vertices[1]->next->degree++;
 		g->nE ++;
 		g->nV = g->nV + 2;
+		// printf("ver %d: eg %d\n",g->nV,g->nE);
 		return 1;
 	}
 	int j = 0;
 	VertexNode *crt;
+	VertexNode *tail;
 	//label to show that if links are added in both vertice side and also indicates the three cases below.
 	//{1,1} means both vertices are existing ones but the edge is new, and it has been added during the while loop, return 1
 	//{1,0}/{0,1} means only one vertex is new and the edge is new, the new vertex and new edge(the link on the new vertex side) have to be added after the while loop, return 1 
@@ -86,61 +96,72 @@ int InsertEdge(Graph g, Edge *e)
 	int isExisting[2] = {0,0};
 	//both vertices are existing, if this function ends in this loop
 	while(g->vertices[j] != NULL){
+		// printf("(%d,%d)-(%d,%d)\n",g->vertices[j]->v->x,g->vertices[j]->v->y,g->vertices[j]->next->v->x,g->vertices[j]->next->v->y);
 		//add link on p1 side
-		if(
-			g->vertices[j]->v->x == e->p1->x && 
-			g->vertices[j]->v->y == e->p1->y
-		){
+		if(g->vertices[j]->v->x == e->p1->x && g->vertices[j]->v->y == e->p1->y){
 			crt = g->vertices[j];
 			//to see if it exists or not
 			while(crt != NULL){
 				//the edge is existing in the graph
 				if(crt->v->x == e->p2->x && crt->v->y == e->p2->y){return 0;}
+				//assign a pointer to the tail of the adjacent list
+				if(crt->next == NULL){
+					tail = crt;
+				}
 				crt = crt->next;
 			}
 			//it is a new edge
-			g->vertices[j]->next = new2;
+			//how to deal with the degree of this newnode??
+			tail->next = NewVertexNode(e->p2);
 			isExisting[0] = 1;
 		}
 		//add link on p2 side
-		if(
-			g->vertices[j]->v->x == e->p2->x && 
-			g->vertices[j]->v->y == e->p2->y
-		){
+		if(g->vertices[j]->v->x == e->p2->x && g->vertices[j]->v->y == e->p2->y){
 			crt = g->vertices[j];
 			while(crt != NULL){
 				//the edge is existing in the graph
 				if(crt->v->x == e->p1->x && crt->v->y == e->p1->y){return 0;}
+				//assign a pointer to the tail of the adjacent list
+				if(crt->next == NULL){
+					tail = crt;
+				}
 				crt = crt->next;
 			}
 			//it is a new edge
-			g->vertices[j]->next = new1;
+			tail->next = NewVertexNode(e->p1);
 			isExisting[1] = 1;
+			// printf("mm(%d,%d)-(%d,%d)\n",g->vertices[j]->v->x,g->vertices[j]->v->y,tail->next->v->x,tail->next->v->y);
 		}
 		//when the links are added in both side, there is no need to continue to go through the loop.
-		if(isExisting[0] == 1 && isExisting[1] == 2){
+		if(isExisting[0] == 1 && isExisting[1] == 1){
 			g->nE++;
+			// printf("no ver %d: eg %d\n",g->nV,g->nE);
 			return 1;
 		}
 		j++;
 	}
 	//one vertices is existing, and another one is new which needed to be added into the array
 	if(isExisting[0] == 1 && isExisting[1] == 0){
-		g->vertices[j] = new2;
-		g->vertices[j]->next = new1;
+		g->vertices[g->nV] = NewVertexNode(e->p2);
+		g->vertices[g->nV]->degree++;
+		g->vertices[g->nV]->next = NewVertexNode(e->p1);
 		g->nV++;
 	}else if(isExisting[0] == 0 && isExisting[1] == 1){
-		g->vertices[j] = new1;
-		g->vertices[j]->next = new2;
+		g->vertices[g->nV] = NewVertexNode(e->p1);
+		g->vertices[g->nV]->degree++;
+		g->vertices[g->nV]->next = NewVertexNode(e->p2);
 		g->nV++;
-	}else{//both are new vertices
-		g->vertices[j] = new1;
-		g->vertices[j]->next = new2;
-		g->vertices[j+1] = new2;
-		g->vertices[j+1]->next = new1;
+	}else {//both are new vertices
+		g->vertices[g->nV] = NewVertexNode(e->p1);
+		g->vertices[g->nV]->degree++;
+		g->vertices[g->nV]->next = NewVertexNode(e->p2);
+		g->vertices[g->nV+1] = NewVertexNode(e->p2);
+		g->vertices[g->nV+1]->degree++;
+		g->vertices[g->nV+1]->next = NewVertexNode(e->p1);
 		g->nV = g->nV + 2;
 	}
 	g->nE++;
+	// printf("ver %d: eg %d\n",g->nV,g->nE);
 	return 1;
 }
 
@@ -171,7 +192,12 @@ void FreeGraph(Graph g)
 // Add the time complexity analysis of ShowGraph() here
 void ShowGraph(Graph g)
 {
-	
+	int j = 0;
+	while(g->vertices[j]!= NULL){
+		printf("(%d,%d)",g->vertices[j]->v->x,g->vertices[j]->v->y);
+		j++;
+	}
+	printf("\n");
 }
 
 int main() //sample main for testing 
@@ -185,7 +211,7 @@ int main() //sample main for testing
   
  // Create first connected component 
  // Insert edge (0,0)-(0,10)
- e_ptr = (Edge*) malloc(sizeof(Edge));
+ e_ptr =(Edge*) malloc(sizeof(Edge));
  assert(e_ptr != NULL);
  v1=(Vertex*) malloc(sizeof(Vertex));
  assert(v1 != NULL);
@@ -198,7 +224,7 @@ int main() //sample main for testing
  e_ptr->p1=v1;
  e_ptr->p2=v2;
  if (InsertEdge(g1, e_ptr)==0) printf("edge exists\n");
- 
+
  // Insert edge (0,0)-(5,6)
  e_ptr = (Edge*) malloc(sizeof(Edge));
  assert(e_ptr != NULL);
@@ -213,7 +239,7 @@ int main() //sample main for testing
  e_ptr->p1=v1;
  e_ptr->p2=v2;
  if (InsertEdge(g1, e_ptr)==0) printf("edge exists\n");
- 
+
  // Insert edge (0, 10)-(10, 10)
  e_ptr = (Edge*) malloc(sizeof(Edge));
  assert(e_ptr != NULL);
@@ -228,7 +254,7 @@ int main() //sample main for testing
  e_ptr->p1=v1;
  e_ptr->p2=v2;
  if (InsertEdge(g1, e_ptr)==0) printf("edge exists\n");
- 
+
  // Insert edge (0,10)-(5,6)
  e_ptr = (Edge*) malloc(sizeof(Edge));
  assert(e_ptr != NULL);
@@ -364,88 +390,89 @@ int main() //sample main for testing
  e_ptr->p1=v1;
  e_ptr->p2=v2;
  if (InsertEdge(g1, e_ptr)==0) printf("edge exists\n"); 
- 
- //Display graph g1
  ShowGraph(g1);
+ 
+//  //Display graph g1
+//  ShowGraph(g1);
 	
- // Find the shortest path between (0,0) and (10,6) 
- v1=(Vertex*) malloc(sizeof(Vertex));
- assert(v1 != NULL);
- v2=(Vertex *) malloc(sizeof(Vertex));
- assert(v2 != NULL);
- v1->x=0;
- v1->y=0;
- v2->x=10;
- v2->y=6;
- ShortestPath(g1, v1, v2);
- free(v1);
- free(v2);
+//  // Find the shortest path between (0,0) and (10,6) 
+//  v1=(Vertex*) malloc(sizeof(Vertex));
+//  assert(v1 != NULL);
+//  v2=(Vertex *) malloc(sizeof(Vertex));
+//  assert(v2 != NULL);
+//  v1->x=0;
+//  v1->y=0;
+//  v2->x=10;
+//  v2->y=6;
+//  ShortestPath(g1, v1, v2);
+//  free(v1);
+//  free(v2);
 	  
- // Delete edge (0,0)-(5, 6)
- e_ptr = (Edge*) malloc(sizeof(Edge));
- assert(e_ptr != NULL);
- v1=(Vertex*) malloc(sizeof(Vertex));
- assert(v1 != NULL);
- v2=(Vertex *) malloc(sizeof(Vertex));
- assert(v2 != NULL);
- v1->x=0;
- v1->y=0;
- v2->x=5;
- v2->y=6;
- e_ptr->p1=v1;
- e_ptr->p2=v2; 	 
- DeleteEdge(g1, e_ptr);
- free(e_ptr);
- free(v1);
- free(v2);
+//  // Delete edge (0,0)-(5, 6)
+//  e_ptr = (Edge*) malloc(sizeof(Edge));
+//  assert(e_ptr != NULL);
+//  v1=(Vertex*) malloc(sizeof(Vertex));
+//  assert(v1 != NULL);
+//  v2=(Vertex *) malloc(sizeof(Vertex));
+//  assert(v2 != NULL);
+//  v1->x=0;
+//  v1->y=0;
+//  v2->x=5;
+//  v2->y=6;
+//  e_ptr->p1=v1;
+//  e_ptr->p2=v2; 	 
+//  DeleteEdge(g1, e_ptr);
+//  free(e_ptr);
+//  free(v1);
+//  free(v2);
  	 
- // Display graph g1
- ShowGraph(g1);
+//  // Display graph g1
+//  ShowGraph(g1);
 	
- // Find the shortest path between (0,0) and (10,6) 
- v1=(Vertex*) malloc(sizeof(Vertex));
- assert(v1 != NULL);
- v2=(Vertex *) malloc(sizeof(Vertex));
- assert(v2 != NULL);
- v1->x=0;
- v1->y=0;
- v2->x=10;
- v2->y=6; 
- ShortestPath(g1, v1, v2);
- free(v1);
- free(v2);
+//  // Find the shortest path between (0,0) and (10,6) 
+//  v1=(Vertex*) malloc(sizeof(Vertex));
+//  assert(v1 != NULL);
+//  v2=(Vertex *) malloc(sizeof(Vertex));
+//  assert(v2 != NULL);
+//  v1->x=0;
+//  v1->y=0;
+//  v2->x=10;
+//  v2->y=6; 
+//  ShortestPath(g1, v1, v2);
+//  free(v1);
+//  free(v2);
  
- // Find the shortest path between (0,0) and (25,5)
- v1=(Vertex*) malloc(sizeof(Vertex));
- assert(v1 != NULL);
- v2=(Vertex *) malloc(sizeof(Vertex));
- assert(v2 != NULL);
- v1->x=0;
- v1->y=0;
- v2->x=25;
- v2->y=5;
- ShortestPath(g1, v1, v2);
- free(v1);
- free(v2);	
+//  // Find the shortest path between (0,0) and (25,5)
+//  v1=(Vertex*) malloc(sizeof(Vertex));
+//  assert(v1 != NULL);
+//  v2=(Vertex *) malloc(sizeof(Vertex));
+//  assert(v2 != NULL);
+//  v1->x=0;
+//  v1->y=0;
+//  v2->x=25;
+//  v2->y=5;
+//  ShortestPath(g1, v1, v2);
+//  free(v1);
+//  free(v2);	
  
- // Find reachable vertices of (0,0)
- v1=(Vertex*) malloc(sizeof(Vertex));
- assert(v1 != NULL);
- v1->x=0;
- v1->y=0;
- ReachableVertices(g1, v1);
- free(v1);
+//  // Find reachable vertices of (0,0)
+//  v1=(Vertex*) malloc(sizeof(Vertex));
+//  assert(v1 != NULL);
+//  v1->x=0;
+//  v1->y=0;
+//  ReachableVertices(g1, v1);
+//  free(v1);
  
- // Find reachable vertices of (20,4)
- v1=(Vertex*) malloc(sizeof(Vertex));
- assert(v1 != NULL);
- v1->x=20;
- v1->y=4;
- ReachableVertices(g1, v1);
- free(v1);
+//  // Find reachable vertices of (20,4)
+//  v1=(Vertex*) malloc(sizeof(Vertex));
+//  assert(v1 != NULL);
+//  v1->x=20;
+//  v1->y=4;
+//  ReachableVertices(g1, v1);
+//  free(v1);
  
- // Free graph g1
- FreeGraph(g1);
+//  // Free graph g1
+//  FreeGraph(g1);
  
  return 0; 
 }
